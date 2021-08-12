8c48334c8608704769da65b1b2d2531079c6fceb
Kamus Tiago <tiagokamus40@gmail.com>
2021-08-12 14:28:11 -0300

    iniciando o projeto

---
+++ src/main/java/com/tiagosilva/crudDeCliente/CrudDeClienteApplication.java
@@ -0,0 +1,13 @@
+package com.tiagosilva.crudDeCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagosilva/crudDeCliente/CrudDeClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagosilva.crudDeCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
